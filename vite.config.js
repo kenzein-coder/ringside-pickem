ce67dd03730e9ee4675bf6e861e4f70f5ab99384
Ziad El-Zein <zelzein@Mac.localdomain>
2025-11-24 13:00:07 +0100

    Increase Vite chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase from default 500KB to 1000KB
+  },
 })
 
